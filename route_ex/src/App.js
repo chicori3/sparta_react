857ad9a802c2b30e6efed442290b57021b50353c
chicori3 <chicori3@naver.com>
2021-01-19 19:42:07 +0900

    Route 2

--- route_ex/src/App.js
+++ route_ex/src/App.js
@@ -1,50 +1,60 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+// Route를 먼저 불러와줍니다.
+// Link 컴포넌트도 불러왔어요.
+import { Route, Link } from "react-router-dom";
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+// 세부 페이지가 되어줄 컴포넌트들도 불러와주고요!
+import Home from "./Home";
+import Cat from "./Cat";
+import Dog from "./Dog";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Link to="/">Home으로 가기</Link>
+          <Link to="/cat">Cat으로 가기</Link>
+          <Link to="/dog">Dog으로 가기</Link>
+        </div>
+
+        <hr />
+        {/* 실제로 연결해볼까요! */}
+        <Route path="/" exact component={Home} />
+        <Route path="/cat" component={Cat} />
+        <Route path="/dog" component={Dog} />
+
+        <button
+          onClick={() => {
+            // props에 있는 history를 사용합니다.
+            // push([이동할 주소])는 페이지를 이동시켜 줍니다.
+            this.props.history.push("/cat");
+          }}
+        >
+          /cat으로 가기
+        </button>
+        <button
+          onClick={() => {
+            // goBack()은 뒤로가기 예요.
+            this.props.history.goBack();
+          }}
+        >
+          뒤로가기
+        </button>
       </div>
-    </Router>
-  );
+    );
+  }
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+// 내보내는 부분에서 withRouter로 감싸줍니다
+export default withRouter(App);
---
+++ route_ex/src/Cat.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cat = (props) => {
+  console.log(props.match);
+
+  return <div>고양이</div>;
+};
+
+export default Cat;
