7565e8000ce838ea03819d0567046835dc81b07c
chicori3 <chicori3@naver.com>
2021-01-22 16:53:51 +0900

    React Redux use

--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -1,20 +1,20 @@
 // 리액트 패키지를 불러옵니다.
 import React from "react";
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
 
 const BucketList = (props) => {
-  console.log(props);
-  const my_lists = props.list;
+  const bucket_list = useSelector((state) => state.bucket.list);
 
   return (
     <ListStyle>
-      {my_lists.map((list, index) => {
+      {bucket_list.map((list, index) => {
         return (
           <ItemStyle
             className="list_item"
             key={index}
             onClick={() => {
-              props.history.push("/detail");
+              props.history.push(`/detail/${index}`);
             }}
           >
             {list}
--- bucket_list/src/Detail.js
+++ bucket_list/src/Detail.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Detail = (props) => {
-  return <div>상세 페이지</div>;
+  const bucket_list = useSelector((state) => state.bucket.list);
+  const bucket_index = parseInt(props.match.params.index);
+
+  return <div>{bucket_list[bucket_index]}</div>;
 };
 
 export default Detail;
