ac10fb868a059a2ffbc21e324a6092109dfe3455
chicori3 <chicori3@naver.com>
2021-01-26 00:28:43 +0900

    KeyFrame ex

--- keyframe-ex/src/App.js
+++ keyframe-ex/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box />
     </div>
   );
 }
+
+const move = keyframes`
+  0% {
+    top:20px;
+    opacity:1;
+    background-color:olive;
+  }
+  
+  50% {
+    top:200px;
+    left:200px;
+    opacity:1;
+    background-color:aquamarine;
+  }
+  
+  100% {
+    left:20px;
+    top:20px;
+    opacity:1;
+    background-color:olive;
+  }
+`;
+
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: olive;
+  border-radius: 150px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  opacity: 1;
+  animation: ${move} 2s 1s infinite;
+`;
 
 export default App;
