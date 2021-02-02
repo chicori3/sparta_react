8b1d37d16e1a354a81eeb593ddc75ade1c870486
chicori3 <chicori3@naver.com>
2021-02-02 14:42:31 +0900

    Middleware 설치

--- bucket_list/src/redux/configStore.js
+++ bucket_list/src/redux/configStore.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import bucket from "./modules/bucket";
 import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
 
+// 미들웨어는 배열로
+const middlewares = [thunk];
+
+const enhancer = applyMiddleware(...middlewares);
 // reducer 합치기 (단일 스토어)
 const rootReducer = combineReducers({ bucket });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
